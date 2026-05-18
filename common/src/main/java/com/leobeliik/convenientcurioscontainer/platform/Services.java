8275b83bcbf9e6afbe907fe71eb8e4c7a2b8605f
Leo <11804768+LeoBeliik@users.noreply.github.com>
2026-05-18 17:33:32 -0300

    Oops, wrong place

--- common/src/main/java/com/leobeliik/convenientcurioscontainer/platform/Services.java
+++ common/src/main/java/com/leobeliik/convenientcurioscontainer/platform/Services.java
@@ -20,9 +20,9 @@ public class Services {
     // manually by including a text file in META-INF/services named with the fully qualified class name of the service.
     // Inside the file you should write the fully qualified class name of the implementation to load for the platform. For
     // example our file on Forge points to ForgePlatformHelper while Fabric points to FabricPlatformHelper.
-    public static <T> T load(Class<T> clazz, Services.class.getClassLoader()) {
+    public static <T> T load(Class<T> clazz) {
 
-        final T loadedService = ServiceLoader.load(clazz)
+        final T loadedService = ServiceLoader.load(clazz, Services.class.getClassLoader())
                 .findFirst()
                 .orElseThrow(() -> new NullPointerException("Failed to load service for " + clazz.getName()));
         LoggerFactory.getLogger(ConvenientCuriosContainerCommon.MODID).debug("Loaded {} for service {}", loadedService, clazz);
