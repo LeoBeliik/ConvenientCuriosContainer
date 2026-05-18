af3fa941556b56ac6715de8148b8d4ba7e983596
LeoBeliik <11804768+LeoBeliik@users.noreply.github.com>
2026-05-18 17:26:54 -0300

    Update Services.java

--- common/src/main/java/com/leobeliik/convenientcurioscontainer/platform/Services.java
+++ common/src/main/java/com/leobeliik/convenientcurioscontainer/platform/Services.java
@@ -20,7 +20,7 @@ public class Services {
     // manually by including a text file in META-INF/services named with the fully qualified class name of the service.
     // Inside the file you should write the fully qualified class name of the implementation to load for the platform. For
     // example our file on Forge points to ForgePlatformHelper while Fabric points to FabricPlatformHelper.
-    public static <T> T load(Class<T> clazz) {
+    public static <T> T load(Class<T> clazz, Services.class.getClassLoader()) {
 
         final T loadedService = ServiceLoader.load(clazz)
                 .findFirst()
